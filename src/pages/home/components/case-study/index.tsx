7346c682f21fe4592fb9dd253505b5bad0b35353
phongkaka <trinhtienphong95@gmail.com>
2023-04-08 09:17:35 +0700

    auto play slider

--- src/pages/home/components/case-study/index.tsx
+++ src/pages/home/components/case-study/index.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
 import Item from "./item";
+
+import { Pagination, Autoplay } from "swiper";
 import "swiper/css";
 import "swiper/css/pagination";
+import "swiper/css/autoplay";
 import "./style.scss";
-
-import { Pagination } from "swiper";
 
 function CaseStudy() {
   return (
@@ -24,14 +25,17 @@ function CaseStudy() {
         </div>
         <div className="slider">
           <Swiper
+            modules={[Pagination, Autoplay]}
             slidesPerView={2}
             spaceBetween={30}
-            centeredSlides={true}
+            centeredSlides
             pagination={{
               clickable: true,
             }}
-            loop={true}
-            modules={[Pagination]}
+            loop
+            autoplay={{
+              delay: 1500,
+            }}
             className="mySwiper"
           >
             <SwiperSlide>
